01189e2676d099a92fc82c70f2f8f666fed9039d
Brian Barbosa <barbosab@users.noreply.github.com>
2022-05-24 17:17:09 -0400

    Added react bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
 import WorldMap from "./components/WorldMap/WorldMap";
 import worldmapjson from "./data/worldmap_lowres.json";
 
 function App() {
   return (
-    <div className="App">
-      <WorldMap geojson={worldmapjson} height={800} width={1000} />
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Pin My Travels</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col style={{ textAlign: "center" }}>
+            <WorldMap geojson={worldmapjson} height={800} width={1200} />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
